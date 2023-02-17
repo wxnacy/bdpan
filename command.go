f94d6f2d788134058ee2989bdc091915acefa43c
xiaoning.wen <xiaoning.wen@yiducloud.cn>
2023-02-17 18:06:15 +0800

    refactor(command): 重构命令添加方式

--- command.go
+++ command.go
@@ -1,51 +1,208 @@
 package main
 
-// import (
-// "fmt"
-// "website/site"
+import (
+	"fmt"
+	"os"
+	"strconv"
 
-// "github.com/akamensky/argparse"
-// )
+	"github.com/akamensky/argparse"
+)
 
-// type Command struct {
-// site.Command
-// // InitCommand          *argparse.Command
-// ListCommand *argparse.Command
-// TestCommand *argparse.Command
+type ICommand interface {
+	Happened() bool
+	Run() error
+}
 
-// ID         *string
-// QueryArgId *string
+func NewCommand(c *argparse.Command) *Command {
+	return &Command{
+		Command: c,
+		AppId: c.String("", "app-id",
+			&argparse.Options{Required: false, Help: "指定添加 App Id"},
+		),
+	}
+
+}
+
+type Command struct {
+	Command *argparse.Command
+	AppId   *string
+}
+
+func (c Command) Happened() bool {
+	return c.Command.Happened()
+}
+
+func NewCommands(parser *argparse.Parser) []ICommand {
+	res := make([]ICommand, 0)
+	res = append(res, NewLoginCommand(parser))
+	res = append(res, NewQueryCommand(parser))
+	return res
+}
+
+// *********************************
+// QueryCommand
+// *********************************
+
+func NewQueryCommand(parser *argparse.Parser) *QueryCommand {
+	c := parser.NewCommand("query", "查询网盘")
+	cmd := &QueryCommand{
+		Command: NewCommand(c),
+	}
+	cmd.Dir = c.String("", "dir",
+		&argparse.Options{Required: false, Help: "查询目录"},
+	)
+	cmd.Name = c.String("n", "name",
+		&argparse.Options{Required: false, Help: "查询名称"},
+	)
+	cmd.FSIDS = c.StringList("", "fsid",
+		&argparse.Options{Required: false, Help: "查询名称"},
+	)
+	return cmd
+}
+
+type QueryCommand struct {
+	*Command
+
+	Dir   *string
+	Name  *string
+	FSIDS *[]string
+}
+
+func (q QueryCommand) Run() error {
+	dir := *q.Dir
+	name := *q.Name
+
+	if len(*q.FSIDS) > 0 {
+		fmt.Println("query fsid")
+		fsids := make([]uint64, 0)
+		for _, fsid := range *q.FSIDS {
+			id, err := strconv.Atoi(fsid)
+			if err != nil {
+				panic(err)
+			}
+			fsids = append(fsids, uint64(id))
+		}
+
+		files, err := GetFilesByFSIDS(fsids)
+		if err != nil {
+			panic(err)
+		}
+		printFileInfoList(files)
+		return nil
+	}
+
+	if name != "" {
+		fmt.Println("query name")
+		res, err := NewFileSearchRequest(name).Dir(dir).Execute()
+		if err != nil {
+			panic(err)
+		}
+		printFileInfoList(res.List)
+		return nil
+	}
+	if dir != "" {
+		fmt.Println("query dir")
+		files, err := GetDirAllFiles(dir)
+		if err != nil {
+			panic(err)
+		}
+		printFileInfoList(files)
+		return nil
+	}
+	return nil
+}
+
+// func test() {
+// var cmd ICommand
+// cmd = &QueryCommand{}
+// fmt.Println(cmd)
 // }
+// *********************************
+// LoginCommand
+// *********************************
+func NewLoginCommand(parser *argparse.Parser) *LoginCommand {
+	c := parser.NewCommand("login", "登录网盘")
+	cmd := &LoginCommand{
+		Command: NewCommand(c),
+	}
+	return cmd
+}
 
-// type ListArg struct {
-// Dir *string
-// }
+type LoginCommand struct {
+	*Command
+}
 
-// var (
-// listArg = ListArg{}
-// )
+func (l LoginCommand) buildCredentail() Credential {
+	appId := *l.AppId
 
-// func NewCommand(parser *argparse.Parser) *Command {
-// c := &Command{Command: *site.NewCommand(parser, "bdpan")}
-// c.ListCommand = c.NewCommand("list", "列出文件")
+	credential := Credential{}
+	fmt.Println("请先完善秘钥信息")
+	if appId == "" {
 
-// listArg.Dir = c.ListCommand.String("", "dir",
-// &argparse.Options{Required: true, Help: "文件名"},
-// )
+		fmt.Print("App Id: ")
+		fmt.Scanln(&credential.AppId)
+	} else {
+		credential.AppId = appId
+	}
+	fmt.Print("App Key: ")
+	fmt.Scanln(&credential.AppKey)
+	fmt.Print("Secret Key: ")
+	fmt.Scanln(&credential.SecretKey)
+	fmt.Print("Sign Key: ")
+	fmt.Scanln(&credential.SignKey)
+	return credential
+}
 
-// return c
-// }
+func (l LoginCommand) Run() error {
+	appId := *l.AppId
+	// var cres []*Credential
+	if appId == "" {
+		_, err = GetCredentails()
+	} else {
+		_, err = GetCredentail(appId)
 
-// func (c *Command) Run() {
+	}
+	if err != nil {
+		credential := l.buildCredentail()
+		err := AddCredentail(credential)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "登录失败 %s\n", err.Error())
+		}
+	}
 
-// if c.ListCommand.Happened() {
-// dto, err := GetDirAllFiles(*listArg.Dir)
-// if err != nil {
-// panic(err)
-// }
-// for _, file := range dto {
-// fmt.Printf("%s\t%d\n", file.Path, file.FSID)
-// }
-// } else if c.TestCommand.Happened() {
-// }
+	config, err := GetConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	var c *Credential
+	if appId != "" {
+		config.LoginAppId = appId
+		c, err = GetCredentail(appId)
+	} else {
+		c, err = GetConfigCredentail()
+	}
+	if err != nil {
+		panic(err)
+	}
+	// err = CreateAccessTokenByDeviceCode()
+	// if err != nil {
+	// panic(err)
+	// }
+	// kt := &AccessToken{}
+	// t.AccessToken = "1"
+	// t.RefreshToken = "1"
+	// saveAccessToken(c.AppId, *t)
+	token, err := c.GetAccessToken()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(*token)
+	return nil
+}
+
+// func test() {
+// var cmd ICommand
+// cmd = &QueryCommand{}
+// fmt.Println(cmd)
 // }
--- main.go
+++ main.go
@@ -8,98 +8,31 @@ import (
 )
 
 func init() {
-	fmt.Println("main init")
-	initArgparse()
 	initAll()
 }
 
 var (
-	err           error
-	testCommand   *argparse.Command
-	loginCommand  *argparse.Command
-	queryCommand  *argparse.Command
-	configCommand *argparse.Command
-
-	loginArg LoginArg
-	queryArg QueryArg
+	err error
 )
-
-func initArgparse() {
-	parser := argparse.NewParser("bdpan", "网站服务")
-
-	configCommand = parser.NewCommand("config", "修改和获取配置")
-	testCommand = parser.NewCommand("test", "测试程序")
-	initLoginArgparse(parser)
-	initQueryArgparse(parser)
-	// // Parse input
-	err = parser.Parse(os.Args)
-	if err != nil {
-		// In case of error print error and print usage
-		// This can also be done by passing -h or --help flags
-		fmt.Print(parser.Usage(err))
-		// panic(parser.Usage(err))
-	}
-}
-
-func initLoginArgparse(parser *argparse.Parser) {
-	loginCommand = parser.NewCommand("login", "登录网盘")
-	loginArg = LoginArg{}
-	loginArg.AppId = loginCommand.String("", "app-id",
-		&argparse.Options{Required: false, Help: "指定添加 App Id"},
-	)
-
-}
-
-type LoginArg struct {
-	AppId *string
-	// AppKey    *string
-	// SecretKey *string
-	// SignKey   *string
-}
-
-type QueryArg struct {
-	AppId *string
-	Dir   *string
-	Name  *string
-	FSIDS *[]string
-	// AppKey    *string
-	// SecretKey *string
-	// SignKey   *string
-}
-
-func initQueryArgparse(parser *argparse.Parser) {
-	queryCommand = parser.NewCommand("query", "查询网盘")
-	queryArg = QueryArg{}
-	queryArg.AppId = queryCommand.String("", "app-id",
-		&argparse.Options{Required: false, Help: "指定添加 App Id"},
-	)
-	queryArg.Dir = queryCommand.String("", "dir",
-		&argparse.Options{Required: false, Help: "查询目录"},
-	)
-	queryArg.Name = queryCommand.String("n", "name",
-		&argparse.Options{Required: false, Help: "查询名称"},
-	)
-	queryArg.FSIDS = queryCommand.StringList("", "fsid",
-		&argparse.Options{Required: false, Help: "查询名称"},
-	)
-}
 
 func main() {
 	fmt.Println("main")
-	if !testCommand.Happened() {
-		_, err = defaultCredentail()
-		if err != nil {
-			fmt.Println("请先执行 bdpan login 进行登录")
+	parser := argparse.NewParser("bdpan", "网站服务")
+	commands := NewCommands(parser)
+	err = parser.Parse(os.Args)
+	if err != nil {
+		fmt.Print(parser.Usage(err))
+		return
+	}
+
+	for _, cmd := range commands {
+		if cmd.Happened() {
+			err = cmd.Run()
+			if err != nil {
+				panic(err)
+			}
 			return
 		}
 	}
-	if testCommand.Happened() {
-		fmt.Println("test")
-	} else if loginCommand.Happened() {
-		Login(loginArg)
-	} else if queryCommand.Happened() {
-		Query(queryArg)
-	} else {
-		fmt.Println("no command")
-	}
+	fmt.Println("没有此命令")
 }
