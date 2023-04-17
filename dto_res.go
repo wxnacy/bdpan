2cf646e19858c70c118fd5cbffbd6529c464f6a8
wxnacy <wxnacy@gmail.com>
2023-04-17 22:53:28 +0800

    feat(dto): 增加 FileInfoDto 部分属性

--- dto_res.go
+++ dto_res.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/wxnacy/gotool"
 )
@@ -41,9 +42,12 @@ type FileInfoDto struct {
 	FSID           uint64 `json:"fs_id"`
 	Path           string `json:"path"`
 	Size           int    `json:"size"`
+	FileType       int    `json:"isdir"`
 	Filename       string `json:"filename"`
 	ServerFilename string `json:"server_filename"`
 	Category       int    `json:"category"`
+	ServerCTime    int64  `json:"server_ctime"`
+	ServerMTime    int64  `json:"server_mtime"`
 	Dlink          string `json:"dlink"`
 	MD5            string `json:"md5"`
 }
@@ -53,6 +57,55 @@ func (f FileInfoDto) GetFilename() string {
 		return f.ServerFilename
 	}
 	return f.Filename
+}
+
+func (f FileInfoDto) formatTime(t int64) string {
+	return time.Unix(t, 0).Format("2006-01-02 15:04:05")
+}
+
+func (f FileInfoDto) GetServerCTime() string {
+	return f.formatTime(f.ServerCTime)
+}
+
+func (f FileInfoDto) GetServerMTime() string {
+	return f.formatTime(f.ServerMTime)
+}
+
+func (f FileInfoDto) IsDir() bool {
+	if f.FileType == 1 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (f FileInfoDto) GetFileType() string {
+	if f.IsDir() {
+		return "目录"
+	} else {
+		return f.GetCategory()
+	}
+}
+
+func (f FileInfoDto) GetCategory() string {
+	// 文件类型，1 视频、2 音频、3 图片、4 文档、5 应用、6 其他、7 种子
+	switch f.Category {
+	case 1:
+		return "视频"
+	case 2:
+		return "音频"
+	case 3:
+		return "图片"
+	case 4:
+		return "文档"
+	case 5:
+		return "应用"
+	case 6:
+		return "其他"
+	case 7:
+		return "种子"
+	}
+	return "未知"
 }
 
 func (f FileInfoDto) PrintOneLine() {
