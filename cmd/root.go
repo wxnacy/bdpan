fbc221158d37be440ee08ee272d570ea8c1e65f0
wxnacy <wxnacy@gmail.com>
2023-04-21 11:07:39 +0800

    feat(cmd): root: 去掉命令报错后额外操作

--- cmd/root.go
+++ cmd/root.go
@@ -6,7 +6,6 @@ package cmd
 
 import (
 	"bdpan"
-	"os"
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -29,10 +28,7 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
-	}
+	rootCmd.Execute()
 }
 
 func init() {
